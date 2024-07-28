6c9fb53836bbcaf9d70263a00045ca8ebdb3a07a
诸征 <23194749@qq.com>
2024-07-28 22:05:37 +0800

    提交

--- market/binance_market.py
+++ market/binance_market.py
@@ -41,5 +41,46 @@ class BinanceMarket:
         result = ...
         AsyncMarket.signals.result.emit(result)
         
-    async def async_get_asset_balance(self, asset):
+    def get_klines(self, symbol, interval, limit, start_time, end_time):
+        """Kline/candlestick bars for a symbol. Klines are uniquely identified by their open time.
+
+        https://binance-docs.github.io/apidocs/spot/en/#kline-candlestick-data
+
+        :param symbol: required
+        :type symbol: str
+        :param interval: -
+        :type interval: str
+        :param limit: - Default 500; max 1000.
+        :type limit: int
+        :param startTime:
+        :type startTime: int
+        :param endTime:
+        :type endTime: int
+
+        :returns: API response
+
+        .. code-block:: python
+
+            [
+                [
+                    1499040000000,      # Open time
+                    "0.01634790",       # Open
+                    "0.80000000",       # High
+                    "0.01575800",       # Low
+                    "0.01577100",       # Close
+                    "148976.11427815",  # Volume
+                    1499644799999,      # Close time
+                    "2434.19055334",    # Quote asset volume
+                    308,                # Number of trades
+                    "1756.87402397",    # Taker buy base asset volume
+                    "28.46694368",      # Taker buy quote asset volume
+                    "17928899.62484339" # Can be ignored
+                ]
+            ]
+
+        :raises: BinanceRequestException, BinanceAPIException
+
+        """
+        
+        data = self.binance_market.get_klines(symbol, interval, limit, start_time, end_time)
         
